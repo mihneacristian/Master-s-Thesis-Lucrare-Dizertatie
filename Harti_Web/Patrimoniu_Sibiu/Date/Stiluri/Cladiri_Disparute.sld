ddf15a39c2fc406e54aea6e192fb63a932e8a6cf
mihneapopa <mihnea-cristian.popa@outlook.com>
2019-10-02 21:47:13 +0300

    Master's Thesis / Lucrare Dizertatie

---
+++ Harti_Web/Patrimoniu_Sibiu/Date/Stiluri/Cladiri_Disparute.sld
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<StyledLayerDescriptor xmlns="http://www.opengis.net/sld" xmlns:ogc="http://www.opengis.net/ogc" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="1.1.0" xmlns:xlink="http://www.w3.org/1999/xlink" xsi:schemaLocation="http://www.opengis.net/sld http://schemas.opengis.net/sld/1.1.0/StyledLayerDescriptor.xsd" xmlns:se="http://www.opengis.net/se">
+  <NamedLayer>
+    <se:Name>Cladiri disparute</se:Name>
+    <UserStyle>
+      <se:Name>Cladiri disparute</se:Name>
+      <se:FeatureTypeStyle>
+        <se:Rule>
+          <se:Name>Single symbol</se:Name>
+          <se:PolygonSymbolizer>
+            <se:Fill>
+              <se:SvgParameter name="fill">#8dd3c7</se:SvgParameter>
+            </se:Fill>
+            <se:Stroke>
+              <se:SvgParameter name="stroke">#000001</se:SvgParameter>
+              <se:SvgParameter name="stroke-width">1</se:SvgParameter>
+              <se:SvgParameter name="stroke-linejoin">bevel</se:SvgParameter>
+            </se:Stroke>
+          </se:PolygonSymbolizer>
+          <se:PolygonSymbolizer>
+            <se:Fill>
+              <se:GraphicFill>
+                <se:Graphic>
+                  <se:Mark>
+                    <se:WellKnownName>line</se:WellKnownName>
+                    <se:Fill>
+                      <se:SvgParameter name="fill">#ff0000</se:SvgParameter>
+                    </se:Fill>
+                    <se:Stroke>
+                      <se:SvgParameter name="stroke">#018571</se:SvgParameter>
+                    </se:Stroke>
+                  </se:Mark>
+                  <se:Size>4</se:Size>
+                </se:Graphic>
+              </se:GraphicFill>
+            </se:Fill>
+            <VendorOption name="distance">7,7</VendorOption>
+          </se:PolygonSymbolizer>
+        </se:Rule>
+      </se:FeatureTypeStyle>
+    </UserStyle>
+  </NamedLayer>
+</StyledLayerDescriptor>
---
+++ Pagina_web/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Istoric()
+        {
+            ViewData["Message"] = "";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+        public IActionResult Evolutie_cladiri()
+        {
+            ViewData["Message"] = "";
+
+            return View();
+        }
+        public IActionResult Sibiul_astazi()
+        {
+            ViewData["Message"] = "";
+
+            return View();
+        }
+        public IActionResult Sibiul_istoric()
+        {
+            ViewData["Message"] = "";
+
+            return View();
+        }
+        public IActionResult Catedrala_Evanghelica()
+        {
+            ViewData["Message"] = "";
+
+            return View();
+        }
+        public IActionResult Sistemul_de_Fortificatii()
+        {
+            ViewData["Message"] = "";
+
+            return View();
+        }
+        public IActionResult Introducere()
+        {
+            ViewData["Message"] = "Aplicație WebGIS pentru promovarea istoriei și patrimoniului orașului Sibiu";
+
+            return View();
+        }
+        public IActionResult Camera_live()
+        {
+            ViewData["Message"] = "";
+
+            return View();
+        }
+        public IActionResult Camera_live_2()
+        {
+            ViewData["Message"] = "";
+        
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
